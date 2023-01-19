dc4f7e1e22f44b9db614932fc50357b16718cfdf
Antonio Victor Melo Trindade <antoniovictormt@hotmail.com>
2023-01-19 15:53:41 -0300

    :tada: First commit

---
+++ server/src/server.ts
@@ -0,0 +1,23 @@
+import cors from "@fastify/cors"
+import { PrismaClient } from '@prisma/client'
+import Fastify from 'fastify'
+
+require('dotenv')
+
+const app = Fastify()
+const prisma = new PrismaClient()
+
+app.register(cors, {
+	origin: process.env.FRONT_END_URL,
+})
+
+app.get('/', async () => {
+	const habits = await prisma.habit.findMany()
+	return habits
+})
+
+app.listen({
+	port: Number(process.env.BACK_END_URL)
+}).then(() => {
+	console.log(`HTTP server listening on port ${process.env.BACK_END_URL}`)
+})
---
+++ web/src/App.tsx
@@ -0,0 +1,15 @@
+import "./styles/global.css";
+import { Habit } from "./components/Habits";
+
+function App() {
+  return (
+    <div>
+      <Habit completed={25} />
+      <Habit completed={50} />
+      <Habit completed={75} />
+      <Habit completed={100} />
+    </div>
+  );
+}
+
+export default App;
